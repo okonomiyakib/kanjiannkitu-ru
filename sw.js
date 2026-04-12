cd5468dfa313cceb70c7dbc9577196931d0f755b
okonomiyakib <raifu.20120405@gmail.com>
2026-04-12 15:59:57 +0900

    漢字得点力PWAアプリ 初回コミット

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'kanji-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon.svg'
+];
+
+// インストール：アセットをキャッシュ
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// アクティベート：古いキャッシュを削除
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// フェッチ：キャッシュ優先（オフライン対応）
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
